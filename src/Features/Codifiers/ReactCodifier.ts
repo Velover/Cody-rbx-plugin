bac8af856a70511f64aa5e62b14a0eaee906aa17
Catargo <cobauper@gmail.com>
2025-05-05 22:55:46 +0200

    - added ImageContent to exceptions - added key-deduplication under the same parent

--- src/Features/Codifiers/ReactCodifier.ts
+++ src/Features/Codifiers/ReactCodifier.ts
@@ -15,7 +15,13 @@ export namespace ReactCodifier {
 		const node_property_exceptions_map = new Map<number, string[]>();
 
 		for (const [, node] of registry) {
-			const property_name_exceptions: string[] = ["Name", "Parent", "Source", "TextureContent"];
+			const property_name_exceptions: string[] = [
+				"Name",
+				"Parent",
+				"Source",
+				"TextureContent",
+				"ImageContent",
+			];
 			node_property_exceptions_map.set(node.Id, property_name_exceptions);
 			//checks for undefined properties
 			//remove name
@@ -79,7 +85,7 @@ export namespace ReactCodifier {
 		registry: Map<number, InstanceAST.IInstanceNode>,
 		property_exceptions_map: Map<number, string[]>,
 	): string {
-		const name = node.Properties.get("Name")!.Value;
+		const name = node.Properties.get("Name")!.Value as string;
 		const has_children = node.ChildrenIds.size() > 0;
 
 		const node_property_exceptions = property_exceptions_map.get(node.Id)!;
@@ -89,8 +95,17 @@ export namespace ReactCodifier {
 		// Start the tag
 		result.push(`<${node.ClassName.lower()}`);
 
+		let nodes_of_same_name_and_smaller_id = 0;
+		for (const [, other_node_id] of registry) {
+			if (other_node_id.ParentId !== node.ParentId) continue;
+			if (other_node_id.Id > node.Id) continue;
+			if (other_node_id.Properties.get("Name")!.Value !== name) continue;
+			nodes_of_same_name_and_smaller_id += 1;
+		}
 		// Add key attribute
-		result.push(` key="${name}"`);
+		result.push(
+			` key="${name + (nodes_of_same_name_and_smaller_id > 0 ? nodes_of_same_name_and_smaller_id : "")}"`,
+		);
 
 		// Add properties
 		for (const prop_name of node.DifferentProperties) {
